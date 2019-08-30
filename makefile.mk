2607e22a1eaeedcad896350f84404a1168601755
SpectrumX <wylieza@gmail.com>
2019-08-30 11:06:53 +0200

    Created makefile

---
+++ makefile.mk
@@ -0,0 +1,22 @@
+JFLAGS = -g
+COMP = javac
+
+.SUFFIXES: .java .class
+
+.java.class:
+	$(COMP) $(JFLAGS) $*.java
+
+CLASSES = \
+	Vector.java \
+	CloudData.java \
+	CloudDataApp.java
+
+default: classes
+
+classes: $(CLASSES:.java=.class)
+
+run:
+	java CloudDataApp
+
+clean:
+	$(RM) *.class
