4262478c007477615b9ceb899b6937e2e42e2b1e
SpectrumX <wylieza@gmail.com>
2019-08-31 19:36:39 +0200

    Created ParCompute to implement parallel computation algorithim

---
+++ ParCompute.java
@@ -0,0 +1,13 @@
+import java.util.concurrent.RecursiveTask;
+
+public class ParCompute extends RecursiveTask<CloudData>{
+
+    public ParCompute(){
+
+    }
+
+    public CloudData compute(){
+        
+    }
+
+}
