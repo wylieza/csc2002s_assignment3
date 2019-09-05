d81510909ffb8a6002f2cdd9b9b7d37f78630cb9
SpectrumX <wylieza@gmail.com>
2019-09-05 13:33:55 +0200

    Added arguments handler

--- CloudDataApp.java
+++ CloudDataApp.java
@@ -1,51 +1,146 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 //package cloudscapes;
 
 public class CloudDataApp{
+    static CloudData data;
+    static int[] ind = new int[3];
+
+    static SeqCompute sequential;
+    static Vector ave;
+
+    static int mode = 0;
     public static void main(String[] args){
-        CloudData data;
-        int[] ind = new int[3];
+        if(args == null){
+            help();
+            System.exit(0);
+        }
+        mode = args.length;
 
-        SeqCompute sequential;
-        Vector ave;
-
-        //SEQUENTIAL EXECUTION
-        if(false){
-            data = new CloudData("largesample_input.txt");
-            sequential = new SeqCompute(data);
-            ave = new Vector();
-
-            System.out.println("Sequential Execution BEGIN");
-            int runs = 3;
-            double[][] times = new double[runs][2];
-            for(int j = 0; j < runs; j++){
-                System.out.println("Test: " + j);
-                System.gc();
-                times[j][0] = System.currentTimeMillis();
-                for(int i = 0; i < data.dim(); i++){
-                    data.locate(i, ind); //Linear position to 3D index conversion
-                    data.classification[ind[0]][ind[1]][ind[2]] = sequential.Classify(i); //Classify current index
-                    
-                }
-                ave = sequential.Average();
-                times[j][1] = System.currentTimeMillis();
-            }        
-            data.writeData("sequential_output.txt", ave);
-            System.out.println("Sequential Execution finnished Sucessfully");
-            for (int i = 0; i < runs; i++){
-                System.out.println("Time " + i + ": " + (times[i][1]-times[i][0])+"");
-            }
+        if(mode == 1){
+            benchmark(args[0]);
         }
 
-        //PARALLEL EXECUTION
-        System.out.println("Parallel Execution BEGIN");
-        data = new CloudData("largesample_input.txt");
-        System.out.println("File Loaded");
+       
+    }
+
+    public static void help(){
+        String help_message = "Cloud Data App\n";
+        help_message += "Arguments [1]: <benchmarking script file name>";
+        help_message += "Arguments [2]: <input data file name> <output data file name>";
+        System.out.println("");
+    }
+
+    public static void benchmark(String fileName){
+       
+        try{ 
+			Scanner sc = new Scanner(new File(fileName), "UTF-8");
+			
+			int numinputf = sc.nextInt();
+            int numseq_cutoffs = sc.nextInt();
+            int seq_cutoff_index = sc.nextInt();
+			String arch = sc.next();
+			
+			String[] inputf = new String[numinputf];
+            int[] seq_cutoffs = new int[numseq_cutoffs];
+            
+				for(int i = 0; i < numinputf; i++){
+                    inputf[i] = sc.next();
+						
+                }
+                for(int i = 0; i < numseq_cutoffs; i++){
+                    seq_cutoffs[i] = sc.nextInt();
+                }
+            sc.close();
+
+            double[][] seq_data = new double[numinputf][2];
+            double[][] parallel_data = new double[numinputf][2];
+
+
+            
+		} 
+		catch (IOException e){ 
+			System.out.println("Unable to open input file "+fileName);
+			e.printStackTrace();
+		}
+		catch (java.util.InputMismatchException e){ 
+			System.out.println("Malformed input file "+fileName);
+			e.printStackTrace();
+		}
+
+    }
+
+    public static void writeFile(double[][] arrdata, String fileName){
+        try{ 
+            FileWriter fileWriter = new FileWriter(fileName);
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            
+            for(int i = 0; i < arrdata.length; i++){
+                    printWriter.printf("%f, %f\n", arrdata[i][0], arrdata[i][1]);
+            }
+           
+           printWriter.close();
+           fileWriter.close(); 
+        }
+        catch (IOException e){
+            System.out.println("Unable to open output file "+fileName);
+               e.printStackTrace();
+        }
+    }
+
+    public static double[] seq_exec(String input_file, String output_file){ //Returns average run time & dim
+        data = new CloudData(input_file);
+        sequential = new SeqCompute(data);
         ave = new Vector();
 
-        ParCompute parallel = new ParCompute(data, 0, data.dim());
-        parallel.fork();       
-        
-        data.writeData("parallel_output.txt", parallel.join());
-        System.out.println("Parallel Execution finnished Sucessfully");
+        System.out.println("Sequential Execution BEGIN");
+        int runs = 8; //Number of runs to do
+        int ignore_first = 3; //Number of runs to ignore due to cache warming
+        double[][] times = new double[runs][2];
+        double[] runtimes = new double[runs];
+        for(int j = 0; j < runs; j++){
+            System.out.println("Test: " + j);
+            System.gc();
+            times[j][0] = System.currentTimeMillis();
+            for(int i = 0; i < data.dim(); i++){
+                data.locate(i, ind); //Linear position to 3D index conversion
+                data.classification[ind[0]][ind[1]][ind[2]] = sequential.Classify(i); //Classify current index
+                
+            }
+            ave = sequential.Average();
+            times[j][1] = System.currentTimeMillis();
+        }        
+        //data.writeData(output_file, ave);
+        System.out.println("Sequential Execution finnished Sucessfully");
+        int sum = 0;
+        for (int i = 0; i < runs; i++){
+            runtimes[i] = (times[i][1]-times[i][0]);
+            if(i >= ignore_first){
+                sum += runtimes[i];
+            }
+            System.out.println("Time " + i + ": " + runtimes[i] +"");
+        }
+        double[] return_arr = new double[2];
+        return_arr[0] = (double) data.dim();
+        return_arr[1] = sum/(double)(runs-ignore_first);        
+        return return_arr;
+    }
+
+    public static double[] par_exec(String input_file, String output_file){
+         //PARALLEL EXECUTION
+         System.out.println("Parallel Execution BEGIN");
+         data = new CloudData(input_file);
+         System.out.println("File Loaded");
+         ave = new Vector();
+ 
+         ParCompute parallel = new ParCompute(data, 0, data.dim());
+         parallel.fork();       
+         
+         //data.writeData(output_file, parallel.join());
+         System.out.println("Parallel Execution finnished Sucessfully");
     }
 }
